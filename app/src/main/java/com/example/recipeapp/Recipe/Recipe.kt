bf734934c460deddb3ece77f4c68982c2c73e326
ahmed sayed <roonya973@gmail.com>
2026-08-26 22:38:55 +0300

    Fix

--- app/src/main/java/com/example/recipeapp/Recipe/Recipe.kt
+++ app/src/main/java/com/example/recipeapp/Recipe/Recipe.kt
@@ -1,10 +1,9 @@
 package com.example.recipeapp.Recipe
-
 data class Recipe(
     val idMeal: String,
     val strMeal: String,
     val strMealThumb: String,
-    val strInstruction: String,
-    val strYoutube: String,
+    val strInstructions: String,
+    val strYoutube: String?,
     var isFavorite: Boolean = false
 )
